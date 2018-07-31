3c2a51469c9852b92daaba5906d1578cd10c032e
kenbuild <542425454@qq.com>
2018-07-31 15:12:26 +0800

    test

--- src/main/java/com/ken/test/Test.java
+++ src/main/java/com/ken/test/Test.java
@@ -3,7 +3,10 @@ package com.ken.test;
 public class Test {
 
     public static void main(String[] args){
+
         System.out.print("a中午123...1123@#$");
+        System.out.print("a中午123...1123@#$");
+
     }
 
 }
